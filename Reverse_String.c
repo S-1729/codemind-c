071456e0d9e16e1cc99becc0b366d08f22247d66
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2022-09-29 21:43:53 +0530

    #include<stdio.h> #include<string.h>
    int main()
    {
        int n,i;
        char a[11];
        scanf("%[^
    ]s",a);
        n=strlen(a);
        for(i=n-1;i>=0;i--)
        {
            printf("%c",a[i]);
        }
    }

---
+++ Reverse_String.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int n,i;
+    char a[11];
+    scanf("%[^
+]s",a);
+    n=strlen(a);
+    for(i=n-1;i>=0;i--)
+    {
+        printf("%c",a[i]);
+    }
+}
