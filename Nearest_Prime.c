9ab8f3c629f650b6797d5be28ad78415e043de84
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2022-10-22 21:56:08 +0530

    #include<stdio.h> #include<math.h> int prime(int i) {     if(i==1 || i==0)     return 0;     int result=sqrt(i),j;     for(j=2;j<=result;j++)     {         if(i%j==0)         return 0;     }     return 1; } int main() {     int n,rem1,k,rem2,t;     scanf("%d",&t);     for(k=0;k<t;k++)     {         scanf("%d",&n);         rem1=0;         while(1)         {             if(prime(n+rem1)==1)             break;             else             rem1+=1;         }         rem2=0;         while(1)         {             if(prime(n-rem2)==1)             break;             else             rem2+=1;         }         if(rem1==rem2)         printf("%d ",n-rem2);         else if(rem1<rem2)         printf("%d ",n+rem1);         else         printf("%d ",n-rem2);     } }

---
+++ Nearest_Prime.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<math.h>
+int prime(int i)
+{
+    if(i==1 || i==0)
+    return 0;
+    int result=sqrt(i),j;
+    for(j=2;j<=result;j++)
+    {
+        if(i%j==0)
+        return 0;
+    }
+    return 1;
+}
+int main()
+{
+    int n,rem1,k,rem2,t;
+    scanf("%d",&t);
+    for(k=0;k<t;k++)
+    {
+        scanf("%d",&n);
+        rem1=0;
+        while(1)
+        {
+            if(prime(n+rem1)==1)
+            break;
+            else
+            rem1+=1;
+        }
+        rem2=0;
+        while(1)
+        {
+            if(prime(n-rem2)==1)
+            break;
+            else
+            rem2+=1;
+        }
+        if(rem1==rem2)
+        printf("%d
+",n-rem2);
+        else if(rem1<rem2)
+        printf("%d
+",n+rem1);
+        else
+        printf("%d
+",n-rem2);
+    }
+}
