6ae8773a0d2b99849006ac3d67cc7e6346081a73
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2022-10-30 10:45:19 +0530

    #include<stdio.h> int main() {     int n,a=0,b=1,c;     c=a+b;     scanf("%d",&n);     while(c<=n)     {         a=b;         b=c;         c=a+b;     }     if((c-n)<(n-b))     printf("%d",c);     else if((c-n)==(n-b))     printf("%d %d",b,c);     else     printf("%d",b); }

--- nearest_fibonacci.c
+++ nearest_fibonacci.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
-#include<math.h>
 int main()
 {
     int n,a=0,b=1,c;
-    scanf("%d",&n);
     c=a+b;
+    scanf("%d",&n);
     while(c<=n)
     {
         a=b;
         b=c;
         c=a+b;
     }
-    if(abs(n-c)<abs(n-b))
-    printf("%d ",c);
-    else if(abs(n-c)==abs(n-b))
+    if((c-n)<(n-b))
+    printf("%d",c);
+    else if((c-n)==(n-b))
     printf("%d %d",b,c);
     else
     printf("%d",b);
