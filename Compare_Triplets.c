a909f7a96e1ffcc48c5589112e901daad72b7de4
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2023-02-04 15:23:17 +0530

    #include<stdio.h> int main() {     int a[3],b[3],c1=0,c2=0;     for(int i=0;i<3;i++)     scanf("%d",&a[i]);     for(int i=0;i<3;i++)     scanf("%d",&b[i]);     for(int i=0;i<3;i++)     {         if(a[i]>b[i])         c1++;         if(a[i]<b[i])         c2++;     }     printf("%d %d",c1,c2); }

---
+++ Compare_Triplets.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a[3],b[3],c1=0,c2=0;
+    for(int i=0;i<3;i++)
+    scanf("%d",&a[i]);
+    for(int i=0;i<3;i++)
+    scanf("%d",&b[i]);
+    for(int i=0;i<3;i++)
+    {
+        if(a[i]>b[i])
+        c1++;
+        if(a[i]<b[i])
+        c2++;
+    }
+    printf("%d %d",c1,c2);
+}
